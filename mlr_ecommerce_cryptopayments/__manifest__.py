9e09b2e9e265ace8d69b2754530918ba841102b9
ERP-FTW <124227412+ERP-FTW@users.noreply.github.com>
2025-11-07 22:32:42 -0500

    Update __manifest__.py

--- mlr_ecommerce_cryptopayments/__manifest__.py
+++ mlr_ecommerce_cryptopayments/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "MLR - MI Lightning Rod",
     "website": "https://www.milightningrod.com",
     "category": "Ecommerce",
-    "version": "17.0",
+    "version": "18.0",
     "depends": ["website", "website_sale"],
     'images': ['static/description/icon.png'],
     "data": [
